34d1e6377f625f20d116c960812c12676c48bc64
TheExpandingPolytope <jesseengerman@hotmail.com>
2024-02-27 08:05:01 -0700

    fix dynamic hostname

--- src/front/src/consts/index.ts
+++ src/front/src/consts/index.ts
@@ -3,4 +3,4 @@ const BASE_URL = `${window.location.protocol}//${window.location.hostname}`;
 export const INSPECTOR_URL = `${BASE_URL}:8080/inspect`;
 export const URL_QUERY_GRAPHQL = `${BASE_URL}:8080/graphql`;
 export const RPC_URL = `${BASE_URL}:8545`;
-export const FAUCET_URL = `$dyna{BASE_URL}:2001/trpc`;
+export const FAUCET_URL = `${BASE_URL}:2001/trpc`;
