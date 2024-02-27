12846994f303ff4ea17f11abc8c77a99bb0a0935
TheExpandingPolytope <jesseengerman@hotmail.com>
2024-02-27 08:01:28 -0700

    dynamic hostname

--- src/front/src/consts/index.ts
+++ src/front/src/consts/index.ts
@@ -1,4 +1,6 @@
-export const INSPECTOR_URL = "http://localhost:8080/inspect";
-export const URL_QUERY_GRAPHQL = "http://localhost:8080/graphql";
-export const RPC_URL = "http://localhost:8545"
-export const FAUCET_URL = "http://0.0.0.0:2001/trpc"
+const BASE_URL = `${window.location.protocol}//${window.location.hostname}`;
+
+export const INSPECTOR_URL = `${BASE_URL}:8080/inspect`;
+export const URL_QUERY_GRAPHQL = `${BASE_URL}:8080/graphql`;
+export const RPC_URL = `${BASE_URL}:8545`;
+export const FAUCET_URL = `$dyna{BASE_URL}:2001/trpc`;
