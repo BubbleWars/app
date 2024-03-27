d7fc95874110618dc67c2f1f77ecb53d0e27a827
TheExpandingPolytope <jesseengerman@hotmail.com>
2024-03-26 20:46:41 -0500

    decrease lerp

--- src/front/src/components/Bubbles.tsx
+++ src/front/src/components/Bubbles.tsx
@@ -100,12 +100,12 @@ export const Bubble = ({ bubbleId }: { bubbleId: string }) => {
         const newX = MathUtils.lerp(
             meshRef.current.position.x,
             bubble.position.x,
-            0.5,
+            0.2,
         );
         const newY = MathUtils.lerp(
             meshRef.current.position.y,
             bubble.position.y,
-            0.5,
+            0.2,
         );
         meshRef.current.position.set(newX, newY, 0);
         meshRef.current.updateMatrix();
