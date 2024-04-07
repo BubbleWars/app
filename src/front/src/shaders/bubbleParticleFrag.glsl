dd6b7893432a80537b45c79378d58743ff5319c0
TheExpandingPolytope <jesseengerman@hotmail.com>
2024-04-07 16:37:23 -0500

    discard alpha< 0.0

--- src/front/src/shaders/bubbleParticleFrag.glsl
+++ src/front/src/shaders/bubbleParticleFrag.glsl
@@ -8,6 +8,9 @@ void main() {
   vec3 color = uColor; // Use the uniform color
   //the lower vDistance is, the more transparent the pixel will be
     float alpha  = 1.0 - vDistance;
+    if (alpha < 0.0) {
+        discard;
+    }
     gl_FragColor = vec4(color, alpha);
   
 }
