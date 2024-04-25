cd4d201bdb321c899c1f9e5d7579dd64e78f5997
TheExpandingPolytope <jesseengerman@hotmail.com>
2024-04-25 14:28:04 -0500

    disable minify

--- src/front/vite.config.ts
+++ src/front/vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     build: {
         target: "esnext",
+        minify: false,
     },
     plugins: [react()],
     define: {
