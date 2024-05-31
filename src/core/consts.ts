32926b16234f2ca999b8da2898d0f2835ac26af8
TheExpandingPolytope <jesseengerman@hotmail.com>
2024-05-31 10:51:07 -0500

    better bounce implementation

--- src/core/consts.ts
+++ src/core/consts.ts
@@ -1,3 +1,5 @@
+import { Settings } from "planck-js";
+
 export const WORLD_WIDTH = 100;
 export const WORLD_HEIGHT = 100;
 export const GRAVITATIONAL_CONSTANT = 0.00001; // 6.67e-11
@@ -19,3 +21,5 @@ export const CLASH_KE = 0.0001;
 export const CLASH_VELOCITY = 2;
 export const PLANCK_MASS = 0.001; 
 export const MIN_DELTA_VELOCITY = 0.1;
+
+Settings.velocityThreshold = 0.05;
