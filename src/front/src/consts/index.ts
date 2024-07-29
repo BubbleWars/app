e60587f4f313253ee483589364d339e77787b3c4
TheExpandingPolytope <jesseengerman@hotmail.com>
2024-07-29 08:08:16 -0500

    generated src lazy upload

--- src/front/src/consts/index.ts
+++ src/front/src/consts/index.ts
@@ -2,7 +2,7 @@ export const BASE_URL = `${window.location.protocol}//${window.location.hostname
 
 const isProd = process.env.NODE_ENV == "production";
 export const INSPECTOR_URL = `${BASE_URL}:8080/inspect`;
-export const URL_QUERY_GRAPHQL = `${BASE_URL}:8080/graphql`;
+export const URL_QUERY_GRAPHQL = isProd ? `${BASE_URL}/graphql` : `${BASE_URL}:8080/graphql`;
 export const RPC_URL =  isProd ? process.env.RPC_HTTP_ENDPOINT :`${BASE_URL}:8545`;
 export const FAUCET_URL = isProd ? `${BASE_URL}/faucet` :`${BASE_URL}:2001/trpc`;
 export const INDEXER_URL = isProd ? `${BASE_URL}/indexer`:`${BASE_URL}:2567`;
